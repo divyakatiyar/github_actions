26f6b63d8a8129a0c4816f96b35c447172c27d0b
divyakatiyar <divyakatiyar2702@gmail.com>
2024-12-30 01:20:50 +0530

    Unit test cases updated

---
+++ test/testops.py
@@ -0,0 +1,10 @@
+from src.mathsop import add, sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def test_sub():
+    assert sub(5,3) == 2
+    assert (3,3)==0
+    assert (2,3)== -1
